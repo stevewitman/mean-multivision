2e706d62dd77b406bb734ff06ddde1df7f29935d
Steve Witman <stevewitman@gmail.com>
2015-03-30 12:52:51 -0600

    ready to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
   stylus = require('stylus'),
   logger = require('morgan'),
   bodyParser = require('body-parser');
+  mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -14,7 +15,10 @@ function compile(str, path) {
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(stylus.middleware(
   {
     src: __dirname + '/public',
@@ -23,10 +27,33 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res) {
-  res.render('index');
+if(env === 'development') {
+  mongoose.connect('mongodb://localhost/multivision');
+} else {
+  mongoose.connect('mongodb://test:test@dbh54.mongolab.com:27547/multivisionsw');
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error ...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
 });
 
-var port = 3030;
+app.get('/partials/:partialPath', function(req, res) {
+  res.render('partials/' + req.params.partialPath);
+})
+
+app.get('*', function(req, res) {
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
+});
+
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + "...");
